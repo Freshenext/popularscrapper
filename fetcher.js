83b58d9d00b35d19573b3ca6cc6cc9130ade5c12
Francis Rodriguez <francisngg@gmail.com>
2022-11-17 15:57:01 -0400

    Re-added insecure http parser

--- fetcher.js
+++ fetcher.js
@@ -5,7 +5,7 @@ const { wrapper } = require('axios-cookiejar-support');
 
 async function fetcher(){
   const jar = new CookieJar();
-  const client = wrapper(axios.create({ jar, validateStatus: () => true }));
+  const client = wrapper(axios.create({ jar, validateStatus: () => true, insecureHTTPParser: true }));
   await client.options(URL, {
     headers: {
       'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:53.0) Gecko/20100101 Firefox/53.0'
