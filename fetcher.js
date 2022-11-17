1d79bba600264f695981e08b8bc47f1fdb66de2a
Francis Rodriguez <francisngg@gmail.com>
2022-11-17 18:12:19 -0400

    Fixed fetcher

--- fetcher.js
+++ fetcher.js
@@ -3,16 +3,21 @@ const { default: axios } = require('axios');
 const { CookieJar } = require('tough-cookie');
 const { wrapper } = require('axios-cookiejar-support');
 
-async function fetcher(){
+async function fetcher() {
   const jar = new CookieJar();
-  const client = wrapper(axios.create({ jar, validateStatus: () => true, insecureHTTPParser: true }));
-  await client.options(URL, {
-    headers: {
-      'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:53.0) Gecko/20100101 Firefox/53.0'
-    }
-  })
+  const client = wrapper(
+    axios.create({
+      jar,
+      validateStatus: () => true,
+      headers: {
+        'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:53.0) Gecko/20100101 Firefox/53.0'
+      }
+    }));
+  await client.get(URL)
   await new Promise((res) => setTimeout(res, 1000))
-  const data =  await client.get(URL)
-  return data
+  await client.get(URL)
+  await new Promise((res) => setTimeout(res, 1000))
+  return await client.get(URL)
 }
+
 module.exports = fetcher
