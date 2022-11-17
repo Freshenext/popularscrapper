ccaa39c2bbe07144b2e224ca3934431fc219ff30
Francis Rodriguez <francisngg@gmail.com>
2022-11-17 15:47:41 -0400

    changed fetcher

--- fetcher.js
+++ fetcher.js
@@ -5,11 +5,8 @@ const { wrapper } = require('axios-cookiejar-support');
 
 async function fetcher(){
   const jar = new CookieJar();
-  const client = wrapper(axios.create({ jar, validateStatus: () => true, headers: {
-      'User-Agent': `Mozilla/5.0 (Linux; Android 12; SM-S906N Build/QP1A.190711.020; wv) AppleWebKit/537.36 (KHTML, like Gecko) Version/4.0 Chrome/80.0.3987.119 Mobile Safari/537.36`
-    }, insecureHTTPParser: true
-  }));
-  await client.get(URL)
+  const client = wrapper(axios.create({ jar, validateStatus: () => true }));
+  await client.options(URL)
   return await client.get(URL)
 }
 module.exports = fetcher
