fedc2a73d03eaf243cd252f5cd02c45092a7f3c8
Francis Rodriguez <francisngg@gmail.com>
2022-11-25 19:08:00 -0400

    Added curl

---
+++ curl.js
@@ -0,0 +1,25 @@
+var axios = require('axios');
+
+var config = {
+  method: 'get',
+  url: 'https://popularenlinea.com/_api/web/lists/getbytitle(\'Rates\')/items?$filter=ItemID%20eq%20%271%27',
+  headers: {
+    'accept': 'application/json; odata=verbose',
+    'accept-encoding': 'application/gzip',
+    'X-Requested-With': 'XMLHttpRequest',
+    'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/107.0.0.0 Safari/537.36',
+    'cache-control': 'no-cache',
+  }
+};
+
+async function runAxios(){
+  try {
+    const response = await axios(config)
+    return response.data
+  } catch (error) {
+    console.log('Error')
+    return {}
+  }
+}
+
+module.exports = runAxios
